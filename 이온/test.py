446cb059b982c3667fbd9ac9b348991ee3fb8a4c
su hyeok <shlee2223@naver.com>
2020-05-15 01:41:25 +0900

    과제 수정

--- 이온/test.py
+++ 이온/test.py
@@ -6,14 +6,16 @@ rank = list(map(int,input("우선순위 입력: ").split()))
 def sort1(rank1 , pick1):
 
     list1 = [c for c in range(len(rank1))]
-    
     list2 = rank1.copy() 
+    print(list1)
+    print(list2)
 
     i = 0
     while True:
-        if list2[i] < max(list2[i+1:]):
+        if list2[i] < max(list2[i:]):
             list1.append(list1.pop(i))
             list2.append(list2.pop(i))
+        
             
         else:
             i += 1
